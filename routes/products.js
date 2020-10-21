08c5cdd552649c196c71f7e2cc5af6545f7571c6
cristianiniguez <felizpc1@gmail.com>
2020-10-21 14:26:05 -0400

    template engines: creando nuestro layout de productos usando pug

--- routes/products.js
+++ routes/products.js
@@ -5,10 +5,14 @@ const products = [
   {
     name: 'Red shoes',
     price: 75,
+    image:
+      'https://images.unsplash.com/photo-1514996242687-4d78a2f38ad4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
   },
   {
     name: 'Black bike',
     price: 300,
+    image:
+      'https://images.unsplash.com/photo-1529929353612-a4320ffeba41?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
   },
 ];
 
