a7fc83b57b68f2e4ef5d0167065f1203fded68e4
cristianiniguez <felizpc1@gmail.com>
2020-10-31 16:31:45 -0400

    deployment: npm script

--- routes/views/products.js
+++ routes/views/products.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const ProductService = require('../../services/products');
 const productService = new ProductService();
 
+const config = require('../../config');
+
 router.get('/', async function (req, res, next) {
   const { tags } = req.query;
   try {
     const products = await productService.getProducts({ tags });
-    res.render('products', { products });
+    res.render('products', { products, dev: config.dev });
   } catch (error) {
     next(error);
   }
